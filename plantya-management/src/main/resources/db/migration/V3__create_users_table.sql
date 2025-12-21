891de7596ca078ecb094d0017c4fffc124cde00e
BleKuntay <riki.mcdougall@hotmail.com>
2025-12-21 14:29:58 +0700

    chore(db): simplify user table schema and adjust sequences
    - Removed default pattern for `user_id` generation from `users` table.
    - Simplified `users` table indexes and timestamps with defaults.
    - Added new `users_seq` sequence for generating user indices.
    - Renamed seed script migration for clarity.

--- plantya-management/src/main/resources/db/migration/V3__create_users_table.sql
+++ plantya-management/src/main/resources/db/migration/V3__create_users_table.sql
@@ -1,29 +1,11 @@
 CREATE TABLE IF NOT EXISTS users (
     id BIGSERIAL PRIMARY KEY,
-    user_id VARCHAR(16) UNIQUE NOT NULL DEFAULT
-        (
-            CONCAT(
-                SUBSTRING(role::text, 1, 1),
-                LPAD(nextval('user_code_seq')::text, 5, '0')
-            )
-        ),    email VARCHAR(255) UNIQUE NOT NULL,
+    user_id VARCHAR(16) UNIQUE,
+    email VARCHAR(255) UNIQUE NOT NULL,
     password VARCHAR(255),
     name VARCHAR(255),
     role user_role NOT NULL DEFAULT 'USER',
-    created_at TIMESTAMP WITH TIME ZONE NOT NULL,
-    updated_at TIMESTAMP WITH TIME ZONE NOT NULL,
-    deleted_at TIMESTAMP WITH TIME ZONE
+    created_at TIMESTAMPTZ NOT NULL DEFAULT now(),
+    updated_at TIMESTAMPTZ NOT NULL DEFAULT now(),
+    deleted_at TIMESTAMPTZ
 );
-
-CREATE INDEX IF NOT EXISTS idx_users_user_id
-    ON users(user_id);
-
-CREATE INDEX IF NOT EXISTS idx_users_email
-    ON users(email);
-
-CREATE INDEX IF NOT EXISTS idx_users_deleted_at
-    ON users(deleted_at);
-
-CREATE INDEX IF NOT EXISTS idx_users_active
-    ON users(id)
-    WHERE deleted_at IS NULL;
